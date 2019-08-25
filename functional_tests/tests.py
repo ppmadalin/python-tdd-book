ce712f084705ae0eeada23b3195541e0c6770601
Madalin Popa <ppmadalin@icloud.com>
2019-08-26 00:09:42 +0300

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -7,10 +8,10 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
@@ -116,7 +117,7 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a  new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
         # Francis visits the home page. There is no sign of Edith's
         # list
